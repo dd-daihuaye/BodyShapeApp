95f9f77755894ba48563453beee0490e7f61a718
Daihua Ye <93221806+dd-daihuaye@users.noreply.github.com>
2023-11-05 13:51:53 -0800

    Initial Commit

---
+++ BodyShapeApp/BodyShapeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyShapeAppApp.swift
+//  BodyShapeApp
+//
+//  Created by Daihua Ye on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct BodyShapeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
